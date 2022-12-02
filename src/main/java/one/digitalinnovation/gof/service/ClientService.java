75b32f1dd03ebf3fc87d8202cfe3e7a0dc290be0
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-12-02 18:06:39 -0300

    Create ClientService.java

---
+++ src/main/java/one/digitalinnovation/gof/service/ClientService.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Client;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ * 
+ * @author falvojr
+ */
+public interface ClientService {
+
+	Iterable<Client> buscarTodos();
+
+	Client buscarPorId(Long id);
+
+	void inserir(Client cliente);
+
+	void atualizar(Long id, Client cliente);
+
+	void deletar(Long id);
+
+}
