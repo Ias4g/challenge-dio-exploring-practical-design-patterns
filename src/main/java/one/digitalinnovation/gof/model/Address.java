e7cf5f3f38b58d434acb8dc47bd85d4455249c3b
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-12-02 18:06:49 -0300

    Update Address.java

--- src/main/java/one/digitalinnovation/gof/model/Address.java
+++ src/main/java/one/digitalinnovation/gof/model/Address.java
@@ -28,4 +28,83 @@ public class Address {
 	private String ddd;
 	private String siafi;
 
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getPublicPlace() {
+		return publicPlace;
+	}
+
+	public void setPublicPlace(String publicPlace) {
+		this.publicPlace = publicPlace;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getLocality() {
+		return locality;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
+
+	public String getGia() {
+		return gia;
+	}
+
+	public void setGia(String gia) {
+		this.gia = gia;
+	}
+
+	public String getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getSiafi() {
+		return siafi;
+	}
+
+	public void setSiafi(String siafi) {
+		this.siafi = siafi;
+	}
 }
